7569e2e127869e5007e93eaee6ea8c7ac9ee42d4
andrea <benedetti.a93@gmail.com>
2018-05-15 11:23:00 +0200

    tooltips, removed old junk

--- killingbees.js
+++ killingbees.js
@@ -8,7 +8,7 @@ let svg = d3.select('#killingbees').append('svg')
 
     // parse values in dataset
     let parseDate = d3.timeParse("%Y-%m-%d");
-    let formatDate = d3.timeFormat("%Y-%m-%d");
+    let formatDate = d3.timeFormat("%Y");
 
     // various scales, could be optimized
     let colors = d3.scaleOrdinal()
@@ -117,7 +117,6 @@ let svg = d3.select('#killingbees').append('svg')
 
       svg.append("g")
       .attr("transform","translate(" + ( width - padding ) + ",0)")
-      .call(healthAxis)
       .classed("yAxis", true);
 
       // Draw circles
@@ -137,12 +136,10 @@ let svg = d3.select('#killingbees').append('svg')
       .force('x', d3.forceX( function(d){
         return x(d[data_setX])
       }).strength(0.99))
-      .force('y', d3.forceY( function(d){
-        return y3(d[data_set])
-      }).strength(0.99))
+      .force('y', d3.forceY( height / 2 ).strength(0.99))
       .force('collide', d3.forceCollide(function(d) { 
         return size(d.kills) + 1 
-      }).iterations(16))
+      }).iterations(32))
       .alphaDecay(0)
       .alpha(0.12)
       .on('tick', tick) 
@@ -151,7 +148,58 @@ let svg = d3.select('#killingbees').append('svg')
       init_decay = setTimeout(function(){
         console.log('init alpha decay')
         simulation.alphaDecay(0.1);
-      }, 10000);
+      }, 5000);
+
+    // tooltip
+    d3.selectAll('.circ').on("mouseenter", function(d){
+
+      d3.selectAll(".circ").style("opacity", 0.2)
+      d3.select(this).style("opacity", 1)
+
+      let tooltip = svg.append("g")
+      .classed("box", true)
+      .attr("transform", "translate(" + ( d.x ) + ", " + ( d.y + 25 ) + ")")
+      .attr("width", 100)
+      .attr("height", 80)
+      .style("background", "white")
+
+      tooltip.append("text")
+      .classed("info", true)
+      .classed("date", true)
+      .text(formatDate(d.value))
+      
+      tooltip.append("text")
+      .classed("info", true)
+      .text(d.kills + " vittime")
+      .attr("transform", "translate(0, " + 12 + ")")
+
+      
+      tooltip.append("text")
+      .classed("info", true)
+      .text("svoltosi " + d.location)
+      .attr("transform", "translate(0, " + 24 + ")")
+
+      tooltip.append("text")
+      .classed("info", true)
+      .text("problemi mentali: " + d.health)
+      .attr("transform", "translate(0, " + 46 + ")")
+
+      tooltip.append("text")
+      .classed("info", true)
+      .text(d.age + " anni")
+      .attr("transform", "translate(0, " + 58 + ")")
+
+      tooltip.append("text")
+      .classed("info", true)
+      .text(d.race)
+      .attr("transform", "translate(0, " + 70 + ")")
+    })
+
+    d3.selectAll('.circ').on("mouseleave", function(d){
+
+      d3.selectAll(".circ").style("opacity", 1)
+      d3.selectAll(".box").remove()
+    })
 
     // Draw UI buttons
 
@@ -162,15 +210,34 @@ let svg = d3.select('#killingbees').append('svg')
     yButtons.append('button').text('genere').attr('value', 'gender').classed('d_sel', true)
     yButtons.append('button').text('etnia').attr('value', 'race').classed('d_sel', true)
 
+    yButtons.append('button').text('X').attr('value', 'race').classed('d_del', true).classed("disabled", true)
+
     let xButtons = d3.select('#killingbees-ui').append('div').classed('buttons', true);
     xButtons.append('p').text('distribuisci per')
     xButtons.append('button').text('età').attr('value', 'age').classed('b_sel', true)
     xButtons.append('button').text('vittime totali').attr('value', 'kills').classed('b_sel', true)
-    xButtons.append('button').text("data dell'attentato").attr('value', 'value').classed('b_sel', true)
-
+    xButtons.append('button').text("data dell'attentato").attr('value', 'value').classed('b_sel', true).style('background','teal').style("color", "white")
 
     // make buttons interactive, vertical categories
     d3.selectAll('.d_sel').on('click', function(){
+
+      yButtons.selectAll('.d_del').style('opacity', 1).classed("disabled", false)
+      d3.selectAll('.d_del').on('click', function(){
+
+        let axisSelection = d3.select(".yAxis")
+        .selectAll("*").remove()
+        .classed("yAxis", true);
+
+        simulation.force('y', d3.forceY( height / 2 ))
+
+        simulation
+        .alphaDecay(0.01)
+        .alpha(0.5)
+        .restart()
+
+        d3.selectAll('.d_sel').classed('selected', false).style('background','transparent')
+        yButtons.selectAll('.d_del').style('opacity', 0).classed("disabled", true)
+      })
 
       d3.selectAll('.d_sel').classed('selected', false).style('background','transparent')
       d3.select(this).classed('selected', true).style('background','tomato')
@@ -182,76 +249,76 @@ let svg = d3.select('#killingbees').append('svg')
       if (data_set === "health") {
         d3.selectAll(".spaceY").text("Stato mentale dell'attentatore")
         let axisSelection = d3.select(".yAxis")
-          .call(healthAxis)
-          .classed("yAxis", true);
+        .call(healthAxis)
+        .classed("yAxis", true);
 
-          axisSelection.selectAll('.tick text')
-          .text(function(d){
-            switch(d){
-              case "Yes":
-                return "malato";
-                break;
-              case "No":
-                return "sano";
-                break;
-              case "Unclear":
-                return "incerto";
-                break;
-              case "Unknown":
-                return "sconosciuto";
-                break;
-            }
-          })
+        axisSelection.selectAll('.tick text')
+        .text(function(d){
+          switch(d){
+            case "Yes":
+            return "malato";
+            break;
+            case "No":
+            return "sano";
+            break;
+            case "Unclear":
+            return "incerto";
+            break;
+            case "Unknown":
+            return "sconosciuto";
+            break;
+          }
+        })
       } else if(data_set === "location") {
         d3.selectAll(".spaceY").text("Tipologia di luogo dell'attentato")
         let axisSelection = d3.select(".yAxis")
-          .call(locationAxis)
-          .classed("yAxis", true);
+        .call(locationAxis)
+        .classed("yAxis", true);
         axisSelection.selectAll('.tick text')
-          .text(function(d){
-            switch(d){
-              case "Open":
-                return "all'aperto";
-                break;
-              case "Open+Close":
-                return "aperto e chiuso";
-                break;
-              case "Close":
-                return "al chiuso";
-                break;
-              case "na":
-                return "non disponibile";
-                break;
-            }
-          })
+        .text(function(d){
+          switch(d){
+            case "Open":
+            return "all'aperto";
+            break;
+            case "Open+Close":
+            return "aperto e chiuso";
+            break;
+            case "Close":
+            return "al chiuso";
+            break;
+            case "na":
+            return "non disponibile";
+            break;
+          }
+        })
       }else if(data_set === "gender") {
         d3.selectAll(".spaceY").text("Genere dell'attentatore")
         let axisSelection = d3.select(".yAxis")
-          .call(genderAxis)
-          .classed("yAxis", true);
+        .call(genderAxis)
+        .classed("yAxis", true);
 
-          axisSelection.selectAll('.tick text')
-          .text(function(d){
-            switch(d){
-              case "Male":
-                return "uomo";
-                break;
-              case "Female":
-                return "donna";
-                break;
-              case "Male/Female":
-                return "uomo e donna";
-                break;
-              case "Unknown":
-                return "non disponibile";
-                break;
-            }
-          })
+        axisSelection.selectAll('.tick text')
+        .text(function(d){
+          switch(d){
+            case "Male":
+            return "uomo";
+            break;
+            case "Female":
+            return "donna";
+            break;
+            case "Male/Female":
+            return "uomo e donna";
+            break;
+            case "Unknown":
+            return "non disponibile";
+            break;
+          }
+        })
       }else {
         d3.selectAll(".spaceY").text("Etnia dell'attentatore")
         d3.select(".yAxis")
-          .call(raceAxis)
-          .classed("yAxis", true);
+        .call(raceAxis)
+        .classed("yAxis", true);
       }
 
       simulation.force('y', d3.forceY(function(d){
@@ -272,19 +339,13 @@ let svg = d3.select('#killingbees').append('svg')
       .alpha(0.5)
       .restart()
 
-      // clearTimeout(init_decay);
-
-      // init_decay = setTimeout(function(){
-      //   console.log('init alpha decay');
-      //   simulation.alphaDecay(0.1);
-      // }, 10000);
     })
 
     // make buttons interactive, horizontal values
     d3.selectAll('.b_sel').on('click', function(){
 
-      d3.selectAll('.b_sel').classed('selected', false).style('background','transparent')
-      d3.select(this).classed('selected', true).style('background','teal')
+      d3.selectAll('.b_sel').classed('selected', false).style('background','transparent').style("color", "teal")
+      d3.select(this).classed('selected', true).style('background','teal').style("color", "white")
 
       data_setX = this.value;
 
@@ -293,18 +354,18 @@ let svg = d3.select('#killingbees').append('svg')
       if (data_setX === "value") {
         d3.selectAll(".spaceX").text("data dell'evento")
         d3.select(".xAxis")
-          .call(dateAxis)
-          .classed("xAxis", true);
+        .call(dateAxis)
+        .classed("xAxis", true);
       }else if(data_setX === "age") {
         d3.selectAll(".spaceX").text("età dell'attentatore")
         d3.select(".xAxis")
-          .call(ageAxis)
-          .classed("xAxis", true);
+        .call(ageAxis)
+        .classed("xAxis", true);
       }else {
         d3.selectAll(".spaceX").text("numero di vittime")
         d3.select(".xAxis")
-          .call(killAxis)
-          .classed("xAxis", true);
+        .call(killAxis)
+        .classed("xAxis", true);
       }
 
       simulation.force('x', d3.forceX(function(d){
@@ -321,13 +382,6 @@ let svg = d3.select('#killingbees').append('svg')
       .alphaDecay(0.01)
       .alpha(0.5)
       .restart()
-
-      // clearTimeout(init_decay);
-
-      // init_decay = setTimeout(function(){
-      //   console.log('init alpha decay');
-      //   simulation.alphaDecay(0.1);
-      // }, 10000);
     })
 
   })
