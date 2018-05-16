b4984707cb97128f92ea7b099d5b5f8e94f42f0d
andrea <benedetti.a93@gmail.com>
2018-05-16 17:21:40 +0200

    updates

--- celeb.js
+++ celeb.js
@@ -27,18 +27,34 @@ d3.tsv("sample.tsv", function(error, data) {
 
 	glyph.append("circle")
 	.attr("cx", 35)
-	.attr("cy", function(d) { return sizeGlyph(d.kills) })
-	.attr("r", function(d) { return sizeGlyph(d.kills) })
+	.attr("cy", function(d) { return size(d.kills) + 2 })
+	.attr("r", function(d) { return size(d.kills) })
 	.attr("fill", function(d) { return colors(d.health) })
 	.classed("highEvents", true)
-	.attr("stroke", "rgba(0,0,0,.5)")
+	.attr("stroke", "rgba(0, 0, 0, .5)")
 	.attr("stroke-width", 1)
+
+	glyph.append("text")
+	.attr("x", 29)
+	.attr("y", 35)
+	.classed("gender", true)
+	.text(function(d) {
+		if(d.gender === "Male") {
+			return "M"
+		} else if(d.gender === "Male/Female") {
+			return "B"
+		} else {
+			return "F"
+		}
+	})
+	.attr("fill", "black")
 
 	container.append("p")
 	.classed("year", true)
 	.text(function(d) {
 		return d.year
 	})
+	
 
 	container.append("p")
 	.classed("location", true)
