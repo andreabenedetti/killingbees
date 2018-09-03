d62f5644706a0b18de1abe72ffba7404e048fb7c
andrea <benedetti.a93@gmail.com>
2018-09-03 11:25:24 +0200

    better timeline

--- js/legs.js
+++ js/legs.js
@@ -1,6 +1,6 @@
 let billsWidth = d3.selectAll("#legislations-timeline").node().getBoundingClientRect().width,
-billsHeight = 300,
-billsPadding = 50;
+billsHeight = 250,
+billsPadding = 100;
 
 let parseTimeline = d3.timeParse("%Y");
 
@@ -9,11 +9,15 @@ let timeline = d3.scaleTime()
 
 let typology = d3.scalePoint()
 .domain(["act","bill","extra","law","none"])
-.range([30, billsHeight - 30])
+.range([30, billsHeight - 0])
+
+let order = d3.scaleLinear()
+.domain([1,33])
+.range([billsHeight - 30, 15])
 
 let locality = d3.scaleOrdinal()
 .domain(["federal","local","none"])
-.range(["#EA1515","#9fc6c3","#FFFFFF"])
+.range(["#010A0A","#999999","#FFFFFF"])
 
 let timelineAxis = d3.axisBottom(timeline).ticks(20)
 .tickFormat(d3.format(",d"))
@@ -22,6 +26,8 @@ let timelineAxis = d3.axisBottom(timeline).ticks(20)
 let bills = d3.select('#legislations-timeline').append('svg')
 .attr("width", billsWidth)
 .attr("height", billsHeight);
+
+let billsTooltip = d3.select('.bills-tooltip');
 
 d3.tsv("data/leg.tsv", function(error, data) {
 	if (error) throw error;
@@ -36,20 +42,37 @@ d3.tsv("data/leg.tsv", function(error, data) {
 	.call(timelineAxis)
 	.classed("timelineAxis", true);
 
-	bills.selectAll("circle")
+	bills.selectAll("rect")
 	.data(data)
 	.enter()
-	.append("circle")
-	.attr("cx", function(d){ return timeline(+d.year) })
-	.attr("cy", d => { return typology(d.type)})
-	.attr("r", 3)
+	.append("rect")
+	.attr("x", d => { return timeline(+d.year) - 3 })
+	.attr("y", d => { return order(d.id)})
+	.attr("width", 7)
+	.attr("height", 7)
 	.attr("fill", d => { return locality(d.where)})
 	.style("opacity", 1)
+	.style("stroke", "black")
 	.classed("bills", true)
 
 	d3.selectAll('.bills').on("mouseenter", function(d){
-		console.log(d.type)
-		console.log(d.info)
+		d3.selectAll(".bills").attr("fill", "none")
+		d3.select(this).attr("fill", d => { return locality(d.where)})
+
+		billsTooltip.append("p")
+		.classed("location", true)
+		.text(d.year + ", " + d.where + " " + d.type)
+
+		billsTooltip.append("p")
+		.classed("summary", true)
+		.text(d.info)
+	});
+
+	d3.selectAll('.bills').on("mouseleave", function(d){
+
+		d3.selectAll(".bills").attr("fill", d => { return locality(d.where)})
+
+		billsTooltip.selectAll("p").remove()
 	});
 
 // console.log(JSON.stringify(data, null, "\t"));
